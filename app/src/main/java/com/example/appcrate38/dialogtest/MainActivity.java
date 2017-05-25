94d5f978af0e98594323c031a5c14de4cfd528ed
appcrate38 <appcrate38@appcrate38ui-iMac.local>
2017-05-25 20:50:18 +0900

    #2 Change Items Dialog and Dialog Event Programming

--- app/src/main/java/com/example/appcrate38/dialogtest/MainActivity.java
+++ app/src/main/java/com/example/appcrate38/dialogtest/MainActivity.java
@@ -1,22 +1,36 @@
 package com.example.appcrate38.dialogtest;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+    String[] itemArr={"Ryujehong","Whoru","Miro","Zunba","EscA","Gido","Tobi"};
+    Button butDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button butDialog=(Button)findViewById(R.id.but_dialog);
+        butDialog=(Button)findViewById(R.id.but_dialog);
         butDialog.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setTitle("첫번째 다이얼로그");
+        dialog.setIcon(R.drawable.lunaticlogo);
+       // dialog.setMessage("여기는 메세지를 쓰는 곳입니다.");
+        dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                butDialog.setText(itemArr[i]);
+            }
+        });
+        dialog.setPositiveButton("OK",null);
+        dialog.show();
     }
 }
