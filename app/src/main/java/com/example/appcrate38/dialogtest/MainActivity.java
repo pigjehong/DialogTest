4f699ba1c6a88f4a6a5f1200cde2145051dbaa04
appcrate38 <appcrate38@appcrate38ui-iMac.local>
2017-05-29 19:14:37 +0900

    #5 checkbox

--- app/src/main/java/com/example/appcrate38/dialogtest/MainActivity.java
+++ app/src/main/java/com/example/appcrate38/dialogtest/MainActivity.java
@@ -24,13 +24,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         dialog.setTitle("첫번째 다이얼로그");
         dialog.setIcon(R.drawable.lunaticlogo);
        // dialog.setMessage("여기는 메세지를 쓰는 곳입니다.");
-        dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+        //단순목록
+        /*dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                butDialog.setText(itemArr[i]);
+            }
+        });*/
+            //라디오 버튼 목록
+        dialog.setSingleChoiceItems(itemArr,0, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int i) {
                 butDialog.setText(itemArr[i]);
             }
         });
-        dialog.setPositiveButton("OK",null);
+        //체크박스 목록
+        boolean[] checkedItems={true,false,true,false,true,true,true};
+        dialog.setMultiChoiceItems(itemArr, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                if(b) {
+                    butDialog.setText(itemArr[i]);
+                }
+            }
+        });
+       // dialog.setPositiveButton("OK",null);
         dialog.show();
     }
 }
